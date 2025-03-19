d0d690fb8e47cce6ab26b6e31f69b8887b3db2b5
Sara <perezsara398@gmail.com>
2025-03-19 18:06:38 -0500

    Tercer commit

--- script.js
+++ script.js
@@ -1,49 +1,85 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var models = [
+window.onload = () => {
+    const button = document.querySelector('button[data-action="change"]');
+    button.innerText = '﹖';
+
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
         {
-            url: './assets/magnemite/magnemite/scene.gltf',
-            scale: '0.05 0.05 0.05', // Ajustamos tamaño
-            position: '0 0 -3',
-            rotation: '0 180 0',
-            info: 'Magnemite, Lv. 5, HP 10/10',
-        },
-        {
-            url: './assets/articuno/scene.gltf',
-            scale: '0.05 0.05 0.05',
-            position: '0 0 -3',
-            rotation: '0 180 0',
-            info: 'Articuno, Lv. 80, HP 100/100',
-        },
-        {
-            url: './assets/dragonite/scene.gltf',
-            scale: '0.05 0.05 0.05',
-            position: '0 0 -3',
-            rotation: '0 180 0',
-            info: 'Dragonite, Lv. 99, HP 150/150',
+            name: 'Pokèmon',
+            location: {
+                // lat: <your-latitude>,
+                // lng: <your-longitude>,
+            },
         },
     ];
+}
 
-    let currentModelIndex = 0;
+var models = [
+    {
+        url: './assets/magnemite/magnemite/scene.gltf',
+        scale: '0.5 0.5 0.5',
+        info: 'Magnemite, Lv. 5, HP 10/10',
+        rotation: '0 180 0',
+    },
+    {
+        url: './assets/articuno/scene.gltf',
+        scale: '0.2 0.2 0.2',
+        rotation: '0 180 0',
+        info: 'Articuno, Lv. 80, HP 100/100',
+    },
+    {
+        url: './assets/dragonite/scene.gltf',
+        scale: '0.08 0.08 0.08',
+        rotation: '0 180 0',
+        info: 'Dragonite, Lv. 99, HP 150/150',
+    },
+];
 
-    const modelEntity = document.querySelector('#model');
-    const infoText = document.querySelector('#info');
-    const button = document.querySelector('button[data-action="change"]');
-
-    function updateModel() {
-        const model = models[currentModelIndex];
-
-        modelEntity.setAttribute('gltf-model', model.url);
-        modelEntity.setAttribute('position', model.position);
-        modelEntity.setAttribute('rotation', model.rotation);
-        modelEntity.setAttribute('scale', model.scale);
-        infoText.innerText = model.info;
+var modelIndex = 0;
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
     }
 
-    button.addEventListener('click', function () {
-        currentModelIndex = (currentModelIndex + 1) % models.length;
-        updateModel();
-    });
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
 
-    // Cargar el primer modelo al inicio
-    updateModel();
-});
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+
+        setModel(models[modelIndex], model);
+
+        model.setAttribute('animation-mixer', '');
+
+        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+            var entity = document.querySelector('[gps-entity-place]');
+            modelIndex++;
+            var newIndex = modelIndex % models.length;
+            setModel(models[newIndex], entity);
+        });
+
+        scene.appendChild(model);
+    });
+}
