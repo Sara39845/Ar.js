a9e6040e2c6bf94e81730d6aabab37e985b89253
Sara <perezsara398@gmail.com>
2025-03-19 19:03:35 -0500

    Tercer commit

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var models = [
     },
     {
         url: './assets/articuno/scene.gltf',
-        scale: '0.05 0.05 0.05',
+        scale: '0.03 0.03 0.03',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
